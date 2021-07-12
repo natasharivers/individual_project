e65fabd6b089e754a5ea367af7474d467f5d3d8a
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-07-12 15:48:57 -0500

    add functions

--- explore.py
+++ explore.py
@@ -15,3 +15,23 @@ def plot_against_target(df, target, var_list, figsize = (12,6), hue = None):
         sns.regplot(data = df, x = var, y = target, 
                     line_kws={'color': 'orange'})
         plt.show()
+
+############################## Plot Univariate ##############################
+
+
+def explore_univariate(train, cat_vars, quant_vars):
+    for var in cat_vars:
+        explore_univariate_categorical(train, var)
+        print('_________________________________________________________________')
+    for col in quant_vars:
+        p, descriptive_stats = explore_univariate_quant(train, col)
+        plt.show(p)
+        print(descriptive_stats)
+
+############################## Plot Bivariate ##############################
+
+def explore_bivariate(train, target, cat_vars, quant_vars):
+    for cat in cat_vars:
+        explore_bivariate_categorical(train, target, cat)
+    for quant in quant_vars:
+        explore_bivariate_quant(train, target, quant)
